f06a552bdfea3deebb2b8c07501f66eaa62f1ae2
Mouad Debbar <mdebbar@google.com>
2022-12-13 12:35:39 -0500

    Changes to icu4x_bidi_example.mjs

--- icu4x_bidi_example.mjs
+++ icu4x_bidi_example.mjs
@@ -1,33 +1,36 @@
-// This file is part of ICU4X. For terms of use, please see the file
-// called LICENSE at the top level of the ICU4X source tree
-// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
-
 import { ICU4XDataProvider } from "./lib/ICU4XDataProvider.js";
 import { ICU4XBidi } from "./lib/ICU4XBidi.js";
-import { readFileSync } from "fs";
 import { default as wasm } from "./lib/diplomat-wasm.mjs";
 
-const SAMPLE_TEXT = [
-    "א",
-    "ב",
-    "ג",
+const SAMPLE_TEXT_ARRAY = [
     "a",
     "b",
+    "(",
     "c",
-].join();
+    ")",
+];
+const SAMPLE_TEXT = SAMPLE_TEXT_ARRAY.join('');
 
 async function main() {
     const provider = new ICU4XDataProvider(wasm.skiawasm_get_provider(), true, []);
     const bidi = ICU4XBidi.create(provider);
-    const bidiInfo = bidi.for_text(SAMPLE_TEXT);
-
-    console.log("Number of paragraphs:", bidiInfo.paragraph_count());
+    const bidiInfo = bidi.for_text(SAMPLE_TEXT, 1);
 
     const paragraph = bidiInfo.paragraph_at(0);
     const reordered = paragraph.reorder_line(paragraph.range_start(), paragraph.range_end());
 
-    console.log("Input:", SAMPLE_TEXT);
-    console.log("Reordered:", reordered);
+    // Print each character separately on its own line.
+    console.log("Input:");
+    printChars(SAMPLE_TEXT);
+    console.log("");
+    console.log("Reordered:");
+    printChars(reordered);
+}
+
+function printChars(text) {
+    for (let i = 0; i < text.length; i++) {
+        console.log('>>', text[i]);
+    }
 }
 
 main();
