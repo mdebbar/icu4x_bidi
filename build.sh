259cb855ea5bd708eac74c8412810f95dd57b9c1
Mouad Debbar <mdebbar@google.com>
2022-12-13 13:59:03 -0500

    Attempt to fix the rust/wasm build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/bash
+#!/usr/bin/env bash
 # This file is part of ICU4X. For terms of use, please see the file
 # called LICENSE at the top level of the ICU4X source tree
 # (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
@@ -18,9 +18,12 @@ rustup +${ICU4X_NIGHTLY_TOOLCHAIN} component add rust-src
 WASM_STACK_SIZE=1000000
 
 BASEDIR=$(dirname "$(realpath "$0")")
+echo "BASEDIR: ${BASEDIR}"
 
 # Don't regen the postcard data by default; delete the file to regen
 if ! test -f "icu4x_data_skiawasm_bake"; then
+    echo "Build WASM library with empty data"
+
     # Build the WASM library with an empty data provider
     RUSTFLAGS="-Cpanic=abort -Copt-level=s -C link-arg=-zstack-size=${WASM_STACK_SIZE} -Clinker-plugin-lto -Ccodegen-units=1 -C linker=${BASEDIR}/ld.py -C linker-flavor=wasm-ld" cargo +${ICU4X_NIGHTLY_TOOLCHAIN} build \
         -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort \
@@ -28,6 +31,8 @@ if ! test -f "icu4x_data_skiawasm_bake"; then
         --release \
         --package icu_capi_skiawasm \
         --features empty_data
+
+    echo "Regen all data"
 
     # Regen all data
     cargo run --manifest-path ../../provider/datagen/Cargo.toml \
@@ -40,6 +45,8 @@ if ! test -f "icu4x_data_skiawasm_bake"; then
         --out ./icu4x_data_skiawasm_bake
 fi
 
+echo "Build WASM library with baked data"
+
 # Build the WASM library, linking in the bake data
 # TODO: This likely doesn't work if $BASEDIR has spaces
 RUSTFLAGS="-Cpanic=abort -Copt-level=s -C link-arg=-zstack-size=${WASM_STACK_SIZE} -Clinker-plugin-lto -Ccodegen-units=1 -C linker=${BASEDIR}/ld.py -C linker-flavor=wasm-ld" cargo +${ICU4X_NIGHTLY_TOOLCHAIN} build \
@@ -50,7 +57,14 @@ RUSTFLAGS="-Cpanic=abort -Copt-level=s -C link-arg=-zstack-size=${WASM_STACK_SIZ
 
 cp target/wasm32-unknown-unknown/release/icu_capi_skiawasm.wasm icu_capi_skiawasm.wasm
 
+echo "Refresh lib folder"
+
 # Refresh the lib folder
-rm -rf lib
-mkdir -p lib
-cp ../diplomat/js/include/* lib
+
+rm -rf lib/*.js lib/*.d.ts
+
+ICU4X_SHA="1d9c42cd543c08d2218e1fa99ca7fdd26fafe084"
+
+while read file; do
+    curl -sL https://raw.githubusercontent.com/unicode-org/icu4x/${ICU4X_SHA}/ffi/diplomat/js/include/${file} -o lib/${file}
+done < lib/_files.txt
